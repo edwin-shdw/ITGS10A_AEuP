aeef4d8f7ef24a54ccbcae16ac9ad2eb91a7ef51
edwin-shdw <edwin.krivoseev@gmx.de>
2022-11-25 23:35:23 +0100

    fix aufgabe3

--- Datentypen und Operatoren/AufgabenTeil1/aufgabe3.py
+++ Datentypen und Operatoren/AufgabenTeil1/aufgabe3.py
@@ -10,8 +10,8 @@ except:
     sys.exit("Das ist keine gültige Eingabe!")
 
 bill = bill * (1-discount)
-tax_val = bill * TAX
-bill_netto = bill * (1-TAX)
+tax_val = bill / (100 + TAX*100) * (TAX * 100)
+bill_netto = bill - tax_val
 
 print("\nIhr Ergebnis: ")
-print(f"Nettobetrag: {bill_netto}\nUmsatzsteuer: {tax_val}\nBruttobetrag: {bill}")
+print(f"Nettobetrag: {bill_netto:.2f}\nUmsatzsteuer: {tax_val:.2f}\nBruttobetrag: {bill:.2f}")
